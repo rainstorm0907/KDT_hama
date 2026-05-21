84ca3db17c77d6360430123b29a313892252a7df
dlwnsgh1130 <dlwnsgh1130@bu.ac.kr>
2026-05-21 18:45:28 +0900

    refactor: 팀원 구조에 맞춰 백엔드 폴더 구조 정리 및 내 코드 이동

--- src/main/java/com/example/ffff/chatbot/repository/SearchLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ffff.chatbot.repository;
-
-import com.example.ffff.chatbot.entity.SearchLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SearchLogRepository extends JpaRepository<SearchLog, Long> {
-}
